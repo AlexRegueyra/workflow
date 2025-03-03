71ae5de023c4ad640cdd2a053904f79e70848f2e
Alex Regueyra <bregueyra@nafin.gob.mx>
2025-03-03 17:18:00 -0600

    refactor(main):eliminacion de codigo no utilizado

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,3 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//     <React.StrictMode>
-//         <App />
-//     </React.StrictMode>,
-// )
-
-// src/main.jsx (o index.js dependiendo de tu configuración)
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './App';
-// import { NotificationProvider } from './contexts/NotificationContext';
-// import './index.css';
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//     <React.StrictMode>
-//         <NotificationProvider>
-//             <App />
-//         </NotificationProvider>
-//     </React.StrictMode>
-// );
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
